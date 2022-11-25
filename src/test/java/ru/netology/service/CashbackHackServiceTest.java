c8f0cca9122e21863b80f330611b515e79387c29
Gleb <g.klochkov@worksolutions.ru>
2022-11-26 01:18:36 +0300

    All test

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void remain100Test() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 900;
+        int amount = 100;
+        int result = service.remain(amount);
+
+        assertEquals(expected, result);
+    }
+    @Test
+    public void remain1000Test() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int amount = 1000;
+        int result = service.remain(amount);
+
+        assertEquals(expected, result);
+    }
+    @Test
+    public void remain10000Test() {
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int amount = 10000;
+        int result = service.remain(amount);
+
+        assertEquals(expected, result);
+    }
+}
